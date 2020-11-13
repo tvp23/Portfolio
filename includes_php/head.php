e7c82d5e73b1bfc0c5145d53e786d69db3b47fdd
Twan van Paridon <70744506+tvp23@users.noreply.github.com>
2020-11-13 08:55:23 +0100

    Porfolio #2

---
+++ includes_php/head.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="includes_css/style.css">
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+    <title>Bruhhhhportfolio</title>
+</head>
+<body>
+
+<header>
+    <div id="block1head">   
+        <div id="block1headA"><img id="logo" src="includes_css/img/logo.png" alt="logo"></div>
+        <div id="block1headB">        
+            <p id="block1headBtext">My account</p>
+            <form>
+                <button id="searchheadbutton" type="submit"><i class="fa fa-search"></i></button>
+                <input id="searchhead" type="text" id="product_search" placeholder="search products..."></input>
+            </form>
+        </div>
+    </div>
+
+    <div id="block2head">
+        <div id="block2headA">
+            <p id="block2headA_item">Shop</p>
+            <p id="block2headA_item">About</p>
+            <p id="block2headA_item">Contact</p>
+        </div>
+        <div id="block2headB">
+            <p id="prizehead">$0,00</p>
+            <p id="itemsincarthead">0 items</p>
+            <i id="basketicon" class='fas fa-shopping-basket'></i>
+        </div>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include "includes_php/head.php"?>
+
+<div id="block1index">
+    <div id="backgroundindex1">
+        <img id="backgroundindex2" src="includes_css/img/hero.jpg">
+        <h2 id="backgroundtext">Welkom</h2>
+    </div>
+</div>
+
+<div id="block2index">
+    <div id="block2indexitems">
+        <div id="imagegridindextext">Shop by category</div>
+        <div id="Imagegridindex">
+            <img id="img1" src="includes_css/img/hoodies.jpg" alt="">
+            <img id="img2" src="includes_css/img/tshirts.jpg" alt="">
+            <img id="img3" src="includes_css/img/accessories.jpg" alt="">
+        </div>
+    </div>
+</div>
